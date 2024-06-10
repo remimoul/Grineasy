55f34b518276aee6977ed856d315360aacb49a4e
Rémi <115152594+remimoul@users.noreply.github.com>
2024-06-10 23:11:56 +0200

    Update LoginScreen component with SafeAreaView, Image, and Button

--- components/MainApp.js
+++ components/MainApp.js
@@ -2,7 +2,7 @@
 import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './LoginScreen';
+import LoginScreen from './HomeScreen';
 import MainContent from './MainContent';
 import { AuthContext } from './AuthProvider';
 
